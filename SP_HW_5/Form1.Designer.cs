4a9a97f6e90204e70abac139bd341ba6cba3977a
Bartash Yriy <66261160+uyriy18@users.noreply.github.com>
2021-04-19 23:03:26 +0300

    Добавьте файлы проекта.

---
+++ SP_HW_5/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SP_HW_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Start_btn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 412);
+            this.panel1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 412);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Start_btn
+            // 
+            this.Start_btn.Location = new System.Drawing.Point(12, 418);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(75, 23);
+            this.Start_btn.TabIndex = 3;
+            this.Start_btn.Text = "Start";
+            this.Start_btn.UseVisualStyleBackColor = true;
+            this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "On the Screen",
+            "In the File"});
+            this.comboBox1.Location = new System.Drawing.Point(93, 420);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Start_btn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Start_btn;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ SP_HW_5/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_HW_5
+{
+    public partial class Form1 : Form
+    {
+        string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+            "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+            "pariatur? Excepteur sint occaecat cupidatat non proident, sunt in " +
+            "culpa qui officia deserunt mollit anim id est laborum!";
+        char[] cText;
+        int sentCount;
+        int wordsCount;
+        int symCount;
+        int interroganiveCount;
+        int exlamationCount;
+        CancellationTokenSource tokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+            tokenSource = new CancellationTokenSource();
+            richTextBox1.Text = text;
+            cText = text.ToCharArray();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            Task task = Task.Factory.StartNew(AnaliseText, tokenSource.Token, tokenSource.Token);
+            task.Wait();
+            if(comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show($"Sentences in the text : {sentCount}\nSymbols in the text : {symCount}\nWords in the text : {wordsCount}\n" +
+                $"Interrogative sentences : {interroganiveCount}\nExlamation sentences {exlamationCount}");
+            }
+           else if(comboBox1.SelectedIndex == 1)
+            {
+                PrintReport();
+            }
+            task.Dispose();
+        }
+        private void AnaliseText(object c)
+        {
+            var cancel = (CancellationToken)c;
+            cancel.ThrowIfCancellationRequested(); 
+            sentCount = CountSentences();
+            symCount = CountSymbols();
+            wordsCount = CountWords();
+            interroganiveCount = CountInterrogative();
+            exlamationCount = CountExlamation();
+
+        }
+        private int CountSentences()
+        {
+            int sCount = 0;
+            foreach(var item in cText)
+            {
+                if (item == '.' || item == '!' || item == '?') sCount++;
+            }
+            return sCount;
+        }
+        private int CountSymbols()
+        {
+            string temp = text.Replace(" ", "");
+            return temp.Length;
+        }
+        private int CountWords()
+        {
+            string[] temp = text.Split(' ');
+            return temp.Length;
+        }
+        private int CountInterrogative()
+        {
+            int result = 0;
+            foreach(var item in cText)
+            {
+                if (item == '?') result++;
+            }
+            return result;
+        }
+        private int CountExlamation()
+        {
+            int result = 0;
+            foreach (var item in cText)
+            {
+                if (item == '!') result++;
+            }
+            return result;
+        }
+        private void PrintReport()
+        {
+            using (SaveFileDialog sd = new SaveFileDialog())
+            {
+                sd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                try
+                {
+                    if (sd.ShowDialog() == DialogResult.OK)
+                    {
+                        using (StreamWriter sw = new StreamWriter(sd.FileName, false, System.Text.Encoding.Default))
+                        {
+                            sw.WriteLine($"Sentences in the text : {sentCount}\nSymbols in the text : {symCount}\nWords in the text : {wordsCount}\n" +
+                            $"Interrogative sentences : {interroganiveCount}\nExlamation sentences {exlamationCount}");
+                        }
+                    }
+                }
+                catch { MessageBox.Show("Error!"); };
+            }
+        }
+    }
+}
